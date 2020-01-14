fc0e2e965d25804003ea530dda6dc774694884dc
[edvernon] <[edvernon@icloud.com]>
2020-01-14 04:37:12 -0800

    First commit added src files

---
+++ pythonCalculator/main.cpp
@@ -0,0 +1,14 @@
+#include "calc.h"
+#include "logger.h"
+
+int main(int argc, char *argv[]) {
+    using namespace ModernCppCI;
+    Logger::level(LogLevel::info);
+
+    Logger log{__func__};
+
+    log.info("doing some calculation");
+    log.info(Calc{} << 1 << "+" << 2 << "*" << 5 << "-" << 3 << "/" << 4);
+
+    return 0;
+}
